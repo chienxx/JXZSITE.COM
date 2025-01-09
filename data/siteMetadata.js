0d8c0dfec5847664af6903b321b68fe134c4a46a
jump2nj <lancertut@yahoo.com>
2025-01-09 11:15:09 +0800

    整理图标

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,8 +1,8 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
   title: '字不如其名',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
+  author: '结弦奏',
+  headerTitle: '字不如其名',
   description: 'A blog created with Next.js and Tailwind.css',
   language: 'en-us',
   theme: 'system', // system, dark or light
@@ -10,18 +10,10 @@ const siteMetadata = {
   siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
   siteLogo: `${process.env.BASE_PATH || ''}/static/images/logo.png`,
   socialBanner: `${process.env.BASE_PATH || ''}/static/images/twitter-card.png`,
-  mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  x: 'https://twitter.com/x',
-  // twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  threads: 'https://www.threads.net',
-  instagram: 'https://www.instagram.com',
-  medium: 'https://medium.com',
-  bluesky: 'https://bsky.app/',
+  email: 'lancertut@yahoo.com',
+  github: 'https://github.com/Jump2nj',
+  spotify: 'https://open.spotify.com/user/31e2umgbxsh6kjdd4vzmex6uggk4',
+  discord: 'https://discord.com/users/1028547869998841876',
   locale: 'en-US',
   // set to true if you want a navbar fixed to the top
   stickyNav: false,
